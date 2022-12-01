8567225a0620e5fe5f1926418a0b27effcc4ad7a
Pacoalberola <falberola2003@gmail.com>
2022-12-01 13:57:29 +0100

    Commit Ejemplo FloatingButton

--- app/src/main/java/com/example/practica3a/MainActivity.kt
+++ app/src/main/java/com/example/practica3a/MainActivity.kt
@@ -12,45 +12,48 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-// Estas dos líneas sustituyen a
-//setContentView(R.layout.activity_main)
+// Estas dos líneas sustituyen a setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
-            var message: String = ""
-            if (binding.editText.text.isEmpty() ||
-                binding.editTextNumber.text.isEmpty()
-            ) {
-                message = "Hay algún texto vacío..."
-                binding.textView.text = ""
-            } else {
-                message = "Texto: " + binding.editText.text.toString() +
-                        "\nNúmero: " + binding.editTextNumber.text.toString()
-                binding.textView.text = message
-                Toast.makeText(
-                    applicationContext,
-                    message,
-                    Toast.LENGTH_SHORT
-                ).show()
+        binding.button.setOnClickListener { actionsButtons("button") }
+        binding.imageButton.setOnClickListener {
+            actionsButtons("imageButton")
+        }
+        binding.fabButton.setOnClickListener { fabActionButton() }
+    }
 
-                ocultarTeclado(it)
-
-                binding.floatingActionButton.setOnClickListener {
-                    Toast.makeText(applicationContext, "Pulsamos el botón flotante",
-                        Toast.LENGTH_LONG).show()
-                }
-
+    private fun actionsButtons(name: String) {
+        when (name) {
+            "button" -> {
+                binding.checkBox.isChecked = true
+                binding.RadioButton1.isChecked = true
+                binding.RadioButton2.isChecked = false
+                binding.toggleButton.isChecked = true
+                binding.switch1.isChecked = true
+            }
+            else -> {
+                binding.checkBox.isChecked = false
+                binding.RadioButton1.isChecked = false
+                binding.RadioButton2.isChecked = true
+                binding.toggleButton.isChecked = false
+                binding.switch1.isChecked = false
             }
         }
     }
 
-    fun ocultarTeclado(view: View?) {
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as
-                InputMethodManager
-        if (view != null) {
-            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-        }
+    private fun fabActionButton() {
+        var texto: String
+        texto = "Pulsados: \n";
+        if (binding.checkBox.isChecked)
+            texto += "CheckBox "
+        if (binding.RadioButton1.isChecked)
+            texto += "Radio 1 "
+        else
+            texto += "Radio 2 "
+        if (binding.toggleButton.isChecked)
+            texto += "Toggle Button "
+        if (binding.switch1.isChecked)
+            texto += "Switch"
+        Toast.makeText(applicationContext, texto, Toast.LENGTH_LONG).show()
     }
-
-
 }
