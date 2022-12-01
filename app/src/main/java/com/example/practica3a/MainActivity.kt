abf8c730697a4c1356826e1b35741ee52699dfa9
Pacoalberola <falberola2003@gmail.com>
2022-12-01 12:49:52 +0100

    Initial commit y Ejemplo

---
+++ app/src/main/java/com/example/practica3a/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.practica3a
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.example.practica3a.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+// Estas dos líneas sustituyen a
+//setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            var message: String = ""
+            if (binding.editText.text.isEmpty() ||
+                binding.editTextNumber.text.isEmpty()
+            ) {
+                message = "Hay algún texto vacío..."
+                binding.textView.text = ""
+            } else {
+                message = "Texto: " + binding.editText.text.toString() +
+                        "\nNúmero: " + binding.editTextNumber.text.toString()
+                binding.textView.text = message
+                Toast.makeText(
+                    applicationContext,
+                    message,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                ocultarTeclado(it)
+            }
+        }
+    }
+
+    fun ocultarTeclado(view: View?) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as
+                InputMethodManager
+        if (view != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+}
