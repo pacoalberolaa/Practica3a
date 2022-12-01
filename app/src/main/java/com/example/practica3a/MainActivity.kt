d775a1c1fdf6f12b65969ac03d7b4c33f9466178
Pacoalberola <falberola2003@gmail.com>
2022-12-01 14:29:52 +0100

    Commit Ejemplo SnackBar

--- app/src/main/java/com/example/practica3a/MainActivity.kt
+++ app/src/main/java/com/example/practica3a/MainActivity.kt
@@ -1,59 +1,57 @@
 package com.example.practica3a
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.example.practica3a.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-// Estas dos líneas sustituyen a setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener { actionsButtons("button") }
-        binding.imageButton.setOnClickListener {
-            actionsButtons("imageButton")
+        binding.btMostrar.setOnClickListener() { mostrarNombre() }
+        binding.btSnackAccion.setOnClickListener() {
+            mostrarSnackBarConAccion()
         }
-        binding.fabButton.setOnClickListener { fabActionButton() }
     }
 
-    private fun actionsButtons(name: String) {
-        when (name) {
-            "button" -> {
-                binding.checkBox.isChecked = true
-                binding.RadioButton1.isChecked = true
-                binding.RadioButton2.isChecked = false
-                binding.toggleButton.isChecked = true
-                binding.switch1.isChecked = true
-            }
-            else -> {
-                binding.checkBox.isChecked = false
-                binding.RadioButton1.isChecked = false
-                binding.RadioButton2.isChecked = true
-                binding.toggleButton.isChecked = false
-                binding.switch1.isChecked = false
+    private fun mostrarNombre() {
+        if (binding.etNombre.text.toString().isEmpty()) {
+            Toast.makeText(this, "Texto vacío", Toast.LENGTH_SHORT).show()
+        } else {
+            if (binding.swSnackBar.isChecked()) {
+                // layoutActividad es el id del Layout en el xml.
+                Snackbar.make(
+                    binding.layoutActividad, binding.etNombre.text,
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(this, binding.etNombre.text, Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun fabActionButton() {
-        var texto: String
-        texto = "Pulsados: \n";
-        if (binding.checkBox.isChecked)
-            texto += "CheckBox "
-        if (binding.RadioButton1.isChecked)
-            texto += "Radio 1 "
-        else
-            texto += "Radio 2 "
-        if (binding.toggleButton.isChecked)
-            texto += "Toggle Button "
-        if (binding.switch1.isChecked)
-            texto += "Switch"
-        Toast.makeText(applicationContext, texto, Toast.LENGTH_LONG).show()
+    private fun mostrarSnackBarConAccion() {
+        // Cambiamos el color de fondo del layout
+        binding.layoutActividad.setBackgroundColor(Color.YELLOW)
+        val snackbar: Snackbar
+        snackbar =
+            Snackbar.make(binding.layoutActividad, "Mi SnackBar con acción!", Snackbar.LENGTH_LONG)
+        // Creamos la acción con nombre y código a ejecutar
+        snackbar.setAction("Deshacer") {
+            // Acciones al pulsar el botón "Deshacer"
+            binding.layoutActividad.setBackgroundColor(Color.WHITE)
+            // Mostramos el Snackbar
+            snackbar.show()
+        }
     }
 }
